3d52bc05c3d0cfeda9fe91bf3878aa3aff191bf6
Mateus Davi Bernart <mateusbernart14@gmail.com>
2026-01-27 23:41:41 -0300

    fix: add id_process in edit fin record

--- app/Services/FinancialRecordService.php
+++ app/Services/FinancialRecordService.php
@@ -11,11 +11,13 @@ class FinancialRecordService
 {
   public function save(array $validated, ?FinancialRecord $financialRecord = null): FinancialRecord
   {
+    $idProcess = ($validated['id_process'] === "0" || empty($validated['id_process'])) ? null : $validated['id_process'];
+
     if (!$financialRecord) {
       $financialRecord = FinancialRecord::create([
         'id_type' => $validated['id_type'],
         'id_category' => $validated['id_category'],
-        'id_process' => $validated['id_process'] === "0" ? null : $validated['id_process'],
+        'id_process' => $idProcess,
         'id_status' => $validated['id_status'],
         'id_user' => auth()->user()->id,
         'description' => $validated['description'],
@@ -25,7 +27,7 @@ class FinancialRecordService
       $financialRecord->update([
         'id_type' => $validated['id_type'],
         'id_category' => $validated['id_category'],
-        'id_process' => $validated['id_process'],
+        'id_process' => $idProcess,
         'id_status' => $validated['id_status'],
         'id_user' => auth()->user()->id,
         'description' => $validated['description'],
